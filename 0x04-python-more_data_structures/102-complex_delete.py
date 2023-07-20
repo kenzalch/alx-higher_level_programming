dc81784005364b734ae89ab6105de073e63f9352
kenzalch <kenzalachgarensa@gmail.com>
2023-07-20 07:53:41 -0700

    updates

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-def square_matrix_map(matrix=[]):
-    return (list(map(lambda x: list(map(lambda y: y**2, x)), matrix)))
-
-
-#!/usr/bin/python3
 def complex_delete(a_dictionary, value):
     list_keys = list(a_dictionary.keys())
 
